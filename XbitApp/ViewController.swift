ef719a23687f38869d117be3c175146b4155e2a7
Santeri Liukkonen <alexander.liukkonen@gmail.com>
2020-03-14 10:38:04 +0100

    Initial Commit

---
+++ XbitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XbitApp
+//
+//  Created by Santeri Liukkonen on 14.03.20.
+//  Copyright © 2020 xbit-ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
