e2cc746c2bd97c16b1ac9ea65b1f5613766aeb12
Santeri Liukkonen <alexander.liukkonen@gmail.com>
2020-03-21 15:55:18 +0100

    Added early layout for detail view

--- XbitApp/DetailView/DetailViewController.swift
+++ XbitApp/DetailView/DetailViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var coverImage: UIImageView!
+    
+    @IBOutlet var phoneImage: UIImageView!
+    
+    @IBOutlet var mailImage: UIImageView!
+    
+    @IBOutlet var scheduleImage: UIImageView!
+    
+    @IBOutlet var profileImage: UIImageView!
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        profileImage.layer.masksToBounds = true
+        profileImage.layer.cornerRadius = profileImage.bounds.width / 2
+        
+        setupSystemImage(image: phoneImage)
+        setupSystemImage(image: mailImage)
+        setupSystemImage(image: scheduleImage)
+        
+    }
+    
+    func setupSystemImage(image: UIImageView) {
+
+        let circleBorder = 1
+        
+        image.clipsToBounds = false
+        image.layer.masksToBounds = false
+        image.layer.cornerRadius  = image.frame.size.height / 2
+        image.layer.borderWidth = CGFloat(circleBorder)
+        image.layer.borderColor = UIColor.systemOrange.cgColor
     }
     
     
-
+    
 }
 
